4f5cddbe5d9b10730502a80cab1f28bd1cc15628
Anton-beep <anton.plyu@gmail.com>
2021-12-22 18:19:52 +0300

    Added: config.cfg, Entities.py, Players.py, Sprites.py, start.py and modified main.py and data, near to alpha ver

--- Entities.py
+++ Entities.py
@@ -1,7 +1,9 @@
 import pygame
 
-from main import SPRITES_GROUPS, CONFIG
+from start import SPRITES_GROUPS, CONFIG
 from Players import Player
+from Sprites import load_images, load_image
+from itertools import cycle
 
 
 class Entity(pygame.sprite.Sprite):
@@ -14,12 +16,73 @@ class Entity(pygame.sprite.Sprite):
         """Increases hp for entity, if self.hp + hp_get > self.max_hp then self.hp = self.max_hp"""
         self.hp = min(self.max_hp, hp_get)
 
-    def get_damage(self, damage: int) -> bool:
+    def get_damage(self, entity, damage: int) -> bool:
         """Decreases hp for entity, if self.hp - damage <= 0 then returns True, which means dead"""
         self.hp -= damage
         return True if self.hp <= 0 else False
 
+    def update(self):
+        pass
+
 
 class Warriors(Entity):
-    def __init__(self, player: Player):
-        super().__init__(SPRITES_GROUPS['ENTITIES'], CONFIG['WarriorsHP'])
+    def __init__(self, spawn_coords: tuple, player: Player):
+        super().__init__(SPRITES_GROUPS['ENTITIES'], CONFIG.getint('warriors', 'HP'),
+                         CONFIG.getint('warriors', 'HPMax'))
+        self.moving_images = cycle(load_images(CONFIG['warriors']['Moving_images']))
+        self.attack_images = cycle(load_images(CONFIG['warriors']['Attacking_images']))
+        self.standing_image = cycle(load_images(CONFIG['warriors']['Standing_images']))
+
+        self.image = next(self.standing_image)
+        self.rect = self.image.get_rect()
+        self.rect.center = spawn_coords
+
+        self.player = player
+        self.target = None
+        self.strength = CONFIG.getint('warriors', 'Strength')
+        self.distance_to_attack = CONFIG.getint('warriors', 'DistanceToAttack')
+
+    def get_damage(self, entity: Entity, damage: int) -> bool:
+        """Gets some damage and if self.target is None then self.target is Entity
+        from which the damage was taken"""
+        if self.target is None:
+            self.target = Entity
+        return super().get_damage(entity, damage)
+
+    def move_to_target(self, speed):
+        """moves self to target"""
+        if type(self.target) == Entity:
+            pass
+        elif type(self.target) == tuple:
+            pass
+
+    def attack_target(self):
+        """attack target and animation"""
+        self.target.get_damage(self, self.strength)
+
+    def set_target(self, target):
+        self.target = target
+
+    def update(self):
+        """moves self to target or attack target, check attacking with collide"""
+        # can use next(<itertools.cycle>) for animation (check itertools.cycle)
+        pass
+
+
+class Tower(Entity):
+    def __init__(self, spawn_coords: tuple, player: Player):
+        super().__init__(SPRITES_GROUPS['ENTITIES'], CONFIG.getint('tower', 'HP'),
+                         CONFIG.getint('tower', 'HPMax'))
+        self.standing_image = load_image('data/tower/standing.png')
+
+        self.image = self.standing_image
+        self.rect = self.image.get_rect()
+        self.rect.center = spawn_coords
+
+        self.player = player
+
+    def update(self):
+        self.image = next(self.standing_image)
+
+
+# Warriors(Player('red')).get_damage(Warriors(Player('blue')), 100)
--- main.py
+++ main.py
@@ -1,23 +1,95 @@
+import sys
+
 import pygame
 import configparser
+from start import *
+from Players import Player, Bot
+from Entities import *
+from Sprites import *
 
-CONFIG = configparser.ConfigParser()
-CONFIG.read('config.cfg')
+PLAYER = Player('blue')
+BOT_ENEMY = Bot('red')
 
-CLOCK = pygame.time.Clock()
-SIZE = list(map(int, [CONFIG['window_size']['WindowWidth'],
-                      CONFIG['window_size']['WindowHeight']]))
-MAIN_SCREEN = pygame.display.set_mode(SIZE)
 
-SPRITES_GROUPS = {
-    'ENTITIES': pygame.sprite.Group(),
-    'STATIC': pygame.sprite.Group()
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    pass
+
+
+def Player_tower(coords):
+    tower = Tower(coords, PLAYER)
+    SPRITES_GROUPS['ENTITIES'].add(tower)
+    ENTITIES_LIST.append(tower)
+
+
+def Bot_tower(coords):
+    tower = Tower(coords, BOT_ENEMY)
+    SPRITES_GROUPS['ENTITIES'].add(tower)
+    ENTITIES_LIST.append(tower)
+
+
+def wall(coords):
+    SPRITES_GROUPS['STATIC'].add(Wall(coords))
+
+
+# sprites dict in level.txt files
+SPRITES_LEVEL = {
+    '.': None,
+    'P': Player_tower,
+    'B': Bot_tower,
+    '#': Wall
 }
+
+
+def load_level(path):
+    """Loads level in SPRITES_GROUPS from path"""
+    if not os.path.isfile(path):
+        print(f'Level {path} not found')
+        terminate()
+    with open(path, 'r', encoding='utf-8') as f:
+        level = list(map(lambda x: x.rstrip(), f.readlines()))
+    max_width = len(max(level, key=lambda x: len(x)))
+    level = list(map(lambda x: list(x) + ['.' for _ in range(len(x), max_width)], level))
+    cell_size = CONFIG.getint('window_size', 'MinCellSize')
+    for row in enumerate(level):
+        for el in enumerate(row[1]):
+            if SPRITES_LEVEL[el[1]] is not None:
+                SPRITES_LEVEL[el[1]](((el[0] + 1) * cell_size, (row[0] + 1) * cell_size))
 
 
 def main():
     pygame.init()
     pygame.display.set_caption('TowerGame')
+
+    # start_screen()
+
+    fps = CONFIG.getint('FPS', 'FPS')
+
+    load_level('data/levels/level1.txt')
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                flag = False
+                for entity in SPRITES_GROUPS['ENTITIES']:
+                    if entity.rect.collidepoint(event.pos):
+                        if entity.get_damage(Entity, 10000000):
+                            entity.kill()
+                        flag = True
+                if not flag:
+                    Warriors(event.pos, PLAYER)
+
+        MAIN_SCREEN.fill(pygame.Color('black'))
+        for group in SPRITES_GROUPS.values():
+            group.draw(MAIN_SCREEN)
+
+        CLOCK.tick(fps)
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
